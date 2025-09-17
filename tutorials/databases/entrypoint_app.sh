30bd2bd74e784e5a6b30f20a9cd8c7391d06db0b
Tom Quareme <tom.quareme@pxl.be>
2025-09-17 16:23:12 +0200

    InfluxDB 3 works in Docker compose

--- tutorials/databases/entrypoint_app.sh
+++ tutorials/databases/entrypoint_app.sh
@@ -5,7 +5,7 @@ set -e
 sleep 5
 
 export INFLUXDB_TOKEN=$(cat /secrets/influxdb_token)
-echo "INFLUXDB_TOKEN=$INFLUXDB_TOKEN"
+# echo "INFLUXDB_TOKEN=$INFLUXDB_TOKEN" # Debug only!
 
 python app.py
 
